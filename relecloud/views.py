c69ea2b0751e7880177e28e34cf36909a2a35c64
ignatejero <103136590+ignatejero@users.noreply.github.com>
2023-12-03 10:27:14 +0100

    Creación archivo views.py

--- relecloud/views.py
+++ relecloud/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from . import models
 from django.views import generic
 from django.contrib.messages.views import SuccessMessageMixin
+from .models import Opinion
+from .forms import OpinionForm
 
 # Create your views here.
 def index(request):
@@ -14,6 +16,25 @@ def about(request):
 def destinations(request):
     all_destinations = models.Destination.objects.all()
     return render(request, 'destinations.html', { 'destinations': all_destinations})
+
+def opinion(request):
+    if request.method == 'POST':
+        form = OpinionForm(request.POST)
+        if form.is_valid():
+            # Guardar la opinión en la base de datos
+            Opinion.objects.create(
+                name=form.cleaned_data['name'],
+                cruise=form.cleaned_data['cruise'],
+                opinion=form.cleaned_data['opinion']
+            )
+            # Redirigir a la misma página después de enviar la opinión
+            return redirect('opinion')
+    else:
+        form = OpinionForm()
+
+    opinions = Opinion.objects.all()
+
+    return render(request, 'opinion.html', {'form': form, 'opinions': opinions})
 
 
 class DestinationDetailView(generic.DetailView):
