48719e857ce05542da86330eb5b6820e30267127
ignatejero <103136590+ignatejero@users.noreply.github.com>
2023-12-03 10:53:46 +0100

    Opción 2 finalizada

--- relecloud/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Cruise
-
-class OpinionForm(forms.Form):
-    name = forms.CharField(label='Nombre', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    cruise = forms.ModelChoiceField(queryset=Cruise.objects.all(), label='Crucero', widget=forms.Select(attrs={'class': 'form-control'}))
-    opinion = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), label='Opinión')
